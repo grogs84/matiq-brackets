28f2aa1f7d54b6906d314549d48a2006489d0b62
matt grogan <johnmattgrogan@gmail.com>
2025-08-08 16:19:04 -0500

    fixed demo look

--- apps/demo/tailwind.config.js
+++ apps/demo/tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "../../packages/matiq-brackets/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
